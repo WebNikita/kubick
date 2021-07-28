a0def220648506c3e1480e3a28d8249970c015e7
Froot <mistergame177@gmail.com>
2021-07-28 20:29:50 +0300

    Тестовый коммит

--- CubickShop/shop/resources.py
+++ CubickShop/shop/resources.py
@@ -24,7 +24,6 @@ class Summer_workwearResource(resources.ModelResource):
 
     class Meta:
         model = Summer_workwear
-        fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protection_class')
 
 
 
@@ -32,118 +31,103 @@ class Winter_workwearResource(resources.ModelResource):
 
    class Meta:
         model = Winter_workwear
-        fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protection_class')
+        # exclude = ("product_ptr", 'category','slug','image')
+        # fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protection_class')
 
 
 class Medical_workwearResource(resources.ModelResource):
 
     class Meta:
         model = Medical_workwear
-        fields = ('name','description','price','size','article','sex','product_type','сomposition')
 
 class Clothing_for_the_service_sectorResource(resources.ModelResource):
 
     class Meta:
         model = Clothing_for_the_service_sector
-        fields = ('name','description','price','size','article','sex','product_type','сomposition')
 
 
 class Protective_clothing_of_security_structuresResource(resources.ModelResource):
 
     class Meta:
         model = Protective_clothing_of_security_structures
-        fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protective_properties')
 
 
 class Special_workwearResource(resources.ModelResource):
 
     class Meta:
         model = Special_workwear
-        fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protection_class')
 
 
 class Signal_workwearResource(resources.ModelResource):
 
     class Meta:
         model = Signal_workwear
-        fields = ('name','description','price','size','article','sex','product_type','сomposition','protection_class')
 
 
 class Protective_protective_workwearResource(resources.ModelResource):
 
     class Meta:
         model = Protective_protective_workwear
-        fields = ('name','description','price','size','article','sex','protective_properties','product_type','сomposition','protection_class')
 
 
 class Clothing_for_hunting_and_fishingResource(resources.ModelResource):
 
     class Meta:
         model = Clothing_for_hunting_and_fishing
-        fields = ('name','description','price','size','article','sex','season','product_type')
 
 
 class KnitweargResource(resources.ModelResource):
 
     class Meta:
         model = Knitwearg
-        fields = ('name','description','price','size','article','sex','product_type')
 
 
 class HatsResource(resources.ModelResource):
 
     class Meta:
         model = Hats
-        fields = ('name','description','price','size','article','sex','product_type','сomposition')
 
 
 class Summer_shoesResource(resources.ModelResource):
 
     class Meta:
         model = Summer_shoes
-        fields = ('name','description','price','size','article','sex','product_type','appointment')
 
 
 class Insulated_shoesResource(resources.ModelResource):
 
     class Meta:
         model = Insulated_shoes
-        fields = ('name','description','price','size','article','sex','product_type','appointment','mounting_method')
 
 
 class Special_insulated_shoesResource(resources.ModelResource):
 
     class Meta:
         model = Special_insulated_shoes
-        fields = ('name','description','price','size','article','sex','product_type','appointment','mounting_method')
 
 
 class PVC_rubber_shoesResource(resources.ModelResource):
 
     class Meta:
         model = PVC_rubber_shoes
-        fields = ('name','description','price','size','article','sex','product_type','appointment','features')
 
 
 class Casual_walking_shoesResource(resources.ModelResource):
 
     class Meta:
         model = Casual_walking_shoes
-        fields = ('name','description','price','size','article','sex','product_type','mounting_method')
 
 
 class Medical_shoesResource(resources.ModelResource):
 
     class Meta:
         model = Medical_shoes
-        fields = ('name','description','price','size','article','sex','product_type','mounting_method')
 
 
 class Shoe_accessoriesResource(resources.ModelResource):
 
     class Meta:
         model = Shoe_accessories
-        fields = ('name','description','price','size','article','sex','product_type','insulation_material')
         # exclude = ('product_ptr','id','category')
 
 
@@ -151,56 +135,48 @@ class Head_and_face_protection_productsResource(resources.ModelResource):
 
     class Meta:
         model = Head_and_face_protection_products
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','mounting_method','features','protective_properties')
 
 
 class Means_of_protection_of_the_organs_of_visionResource(resources.ModelResource):
 
    class Meta:
         model = Means_of_protection_of_the_organs_of_vision
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','lens_coating','features','protective_properties')
 
 
 class Protective_equipment_during_welding_operationsResource(resources.ModelResource):
 
     class Meta:
         model = Protective_equipment_during_welding_operations
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','meets_the_requirements_of_the_FSS','features','protective_properties')
 
 
 class Hearing_protection_equipmentResource(resources.ModelResource):
 
     class Meta:
         model = Hearing_protection_equipment
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','features','protective_properties')
 
 
 class Respiratory_protection_equipmentResource(resources.ModelResource):
 
     class Meta:
         model = Respiratory_protection_equipment
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','meets_the_requirements_of_the_FSS','features','protective_properties')
 
 
 class Protective_equipment_during_highrise_worksResource(resources.ModelResource):
 
     class Meta:
         model = Protective_equipment_during_highrise_works
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','meets_the_requirements_of_the_FSS','features','protective_properties')
 
 
 class Clothing_with_limited_service_lifeResource(resources.ModelResource):
 
     class Meta:
         model = Clothing_with_limited_service_life
-        fields = ('name','description','price','size','article','sex','sub_category','product_type','protective_properties')
 
 
 class Dielectric_safety_devicesResource(resources.ModelResource):
     
     class Meta:
         model = Dielectric_safety_devices
-        fields = ('name','description','price','size','article','sex')
 
 
 
@@ -208,125 +184,106 @@ class Knitted_glovesResource(resources.ModelResource):
 
     class Meta:
         model = Knitted_gloves
-        fields = ('name','description','price','size','article','sex','material','meets_the_requirements_of_the_FSS','features','cuff_Type')
 
 
 class Wool_blend_glovesResource(resources.ModelResource):
 
     class Meta:
         model = Wool_blend_gloves
-        fields = ('name','description','price','size','article','sex','material','meets_the_requirements_of_the_FSS','features','cuff_Type'
-                  ,'appointment','insulation_material','сomposition','protective_properties'  )
 
 
 class Split_gloves_combinedResource(resources.ModelResource):
 
     class Meta:
         model = Split_gloves_combined
-        fields = ('name','description','price','size','article','sex','material','features','appointment','lining','protective_properties')
 
 
 class Kragi_vachegiResource(resources.ModelResource):
 
     class Meta:
         model = Kragi_vachegi
-        fields = ('name','description','price','size','article','sex','material','appointment','lining','protective_properties','product_type')
 
 
 class Specialized_glovesResource(resources.ModelResource):
 
     class Meta:
         model = Specialized_gloves
-        fields = ('name','description','price','size','article','sex','appointment','protection_class','protective_properties','product_type')
 
 
 class Household_gloves_disposableResource(resources.ModelResource):
 
     class Meta:
         model = Household_gloves_disposable
-        fields = ('name','description','price','size','article','sex','appointment','protective_properties')
 
 
 class Working_glovesResource(resources.ModelResource):
 
     class Meta:
         model = Working_gloves
-        fields = ('name','description','price','size','article','sex','material','appointment','lining','protective_properties','product_type','features')
 
 
 class Insulated_mittensResource(resources.ModelResource):
 
     class Meta:
         model = Insulated_mittens
-        fields = ('name','description','price','size','article','sex','features','appointment','insulation_material','protective_properties')
 
 
 class Medical_suppliesResource(resources.ModelResource):
 
     class Meta:
         model = Medical_supplies
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 class Dermatological_agentsResource(resources.ModelResource):
 
     class Meta:
         model = Dermatological_agents
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Technical_fabricsResource(resources.ModelResource):
 
     class Meta:
         model = Technical_fabrics
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Detergents_and_household_chemicalsResource(resources.ModelResource):
 
     class Meta:
         model = Detergents_and_household_chemicals
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 class Firefighting_equipment_fire_extinguishersResource(resources.ModelResource):
 
     class Meta:
         model = Firefighting_equipment_fire_extinguishers
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Protective_equipmentResource(resources.ModelResource):
 
     class Meta:
         model = Protective_equipment
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Household_goodsResource(resources.ModelResource):
 
     class Meta:
         model = Household_goods
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Snow_removal_equipmentResource(resources.ModelResource):
 
     class Meta:
         model = Snow_removal_equipment
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Gardening_toolsResource(resources.ModelResource):
 
     class Meta:
         model = Gardening_tools
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Bristle_and_brush_productsResource(resources.ModelResource):
 
     class Meta:
         model = Bristle_and_brush_products
-        fields = ('name','description','price','size','article','sex','sub_category')
 
 
 class Bed_linen_setsResource(resources.ModelResource):
@@ -334,7 +291,6 @@ class Bed_linen_setsResource(resources.ModelResource):
     
     class Meta:
         model = Bed_linen_sets
-        fields = ('name','description','price','size','article','sex')
 
 
 class MattressesResource(resources.ModelResource):
@@ -342,7 +298,6 @@ class MattressesResource(resources.ModelResource):
     
     class Meta:
         model = Mattresses
-        fields = ('name','description','price','size','article','sex')
 
 
 class BlanketsResource(resources.ModelResource):
@@ -350,7 +305,6 @@ class BlanketsResource(resources.ModelResource):
     
     class Meta:
         model = Blankets
-        fields = ('name','description','price','size','article','sex')
 
 
 class PillowsResource(resources.ModelResource):
@@ -358,7 +312,6 @@ class PillowsResource(resources.ModelResource):
     
     class Meta:
         model = Pillows
-        fields = ('name','description','price','size','article','sex')
 
 
 class Bedspreads_blanketsResource(resources.ModelResource):
@@ -366,7 +319,6 @@ class Bedspreads_blanketsResource(resources.ModelResource):
     
     class Meta:
         model = Bedspreads_blankets
-        fields = ('name','description','price','size','article','sex')
 
 
 class Waffle_towelsResource(resources.ModelResource):
@@ -374,7 +326,6 @@ class Waffle_towelsResource(resources.ModelResource):
     
     class Meta:
         model = Waffle_towels
-        fields = ('name','description','price','size','article','sex')
 
 
 class Terry_towelsResource(resources.ModelResource):
@@ -382,4 +333,3 @@ class Terry_towelsResource(resources.ModelResource):
     
     class Meta:
         model = Terry_towels
-        fields = ('name','description','price','size','article','sex')
