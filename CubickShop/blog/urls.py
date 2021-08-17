04c8b00caaaf80eaac9e2b3076cf7d5886b4ae02
Froot <mistergame177@gmail.com>
2021-08-17 21:06:54 +0300

    Фикс в блоге

--- CubickShop/blog/urls.py
+++ CubickShop/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.all_news, name='blog_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.blog_detail, name='blog_detail'),
     
 ]
