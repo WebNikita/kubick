8571b5ff57b7e9d810c58858d7345af8c89053c8
Froot <mistergame177@gmail.com>
2021-08-20 17:04:24 +0300

    Фильтры готовы, пагинация почти

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView, ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, request
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q
+from django.db.models import F
 
 from cart.forms import CartAddProductForm
 from itertools import chain
@@ -70,20 +70,89 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
         #     return queryset
 
     def get_context_data(self, **kwargs):
+        CT_MODEL_MODEL_CLASS = {
+            'summer_workwear': Summer_workwear,
+            'winter_workwear': Winter_workwear,
+            'medical_workwear': Medical_workwear,
+            'clothing_for_the_service_sector': Clothing_for_the_service_sector,
+            'protective_clothing_of_security_structures': Protective_clothing_of_security_structures,
+            'special_workwear': Special_workwear,
+            'signal_workwear': Signal_workwear,
+            'protective_protective_workwear': Protective_protective_workwear,
+            'clothing_for_hunting_and_fishing': Clothing_for_hunting_and_fishing,
+            'knitwearg': Knitwearg,
+            'hats': Hats,
+            'summer_shoes': Summer_shoes,
+            'insulated_shoes': Insulated_shoes,
+            'special_insulated_shoes': Special_insulated_shoes,
+            'pvc_rubber_shoes': PVC_rubber_shoes,
+            'casual_walking_shoes': Casual_walking_shoes,
+            'medical_shoes': Medical_shoes,
+            'shoe_accessories': Shoe_accessories,
+            'head_and_face_protection_products': Head_and_face_protection_products,
+            'means_of_protection_of_the_organs_of_vision': Means_of_protection_of_the_organs_of_vision,
+            'protective_equipment_during_welding_operations': Protective_equipment_during_welding_operations,
+            'hearing_protection_equipment': Hearing_protection_equipment,
+            'respiratory_protection_equipment': Respiratory_protection_equipment,
+            'protective_equipment_during_highrise_works': Protective_equipment_during_highrise_works,
+            'clothing_with_limited_service_life': Clothing_with_limited_service_life,
+            'dielectric_safety_devices': Dielectric_safety_devices,
+            'knitted_gloves': Knitted_gloves,
+            'wool_blend_gloves': Wool_blend_gloves,
+            'split_gloves_combined': Split_gloves_combined,
+            'kragi_vachegi': Kragi_vachegi,
+            'specialized_gloves': Specialized_gloves,
+            'household_gloves_disposable': Household_gloves_disposable,
+            'working_gloves': Working_gloves,
+            'insulated_mittens': Insulated_mittens,
+            'medical_supplies': Medical_supplies,
+            'dermatological_agents': Dermatological_agents,
+            'technical_fabrics': Technical_fabrics,
+            'detergents_and_household_chemicals': Detergents_and_household_chemicals,
+            'firefighting_equipment_fire_extinguishers': Firefighting_equipment_fire_extinguishers,
+            'protective_equipment': Protective_equipment,
+            'household_goods': Household_goods,
+            'snow_removal_equipment': Snow_removal_equipment,
+            'gardening_tools': Gardening_tools,
+            'bristle_and_brush_products': Bristle_and_brush_products,
+            'bed_linen_sets': Bed_linen_sets,
+            'mattresses': Mattresses,
+            'blankets': Blankets,
+            'pillows': Pillows,
+            'bedspreads_blankets': Bedspreads_blankets,
+            'waffle_towels': Waffle_towels,
+            'terry_towels': Terry_towels,
+        }
+
         slug = self.kwargs['slug']
         context = super().get_context_data(**kwargs)
-        query = self.request.GET
+        query_dict = dict(self.request.GET)
+        filter = {}
+        filter_results = CT_MODEL_MODEL_CLASS[slug].objects.none()
+        
 
-        for key in dict(query).keys():
-            print(f'{key}--->{dict(query)[key]}')
-
-        if len(query) != 0:
-            print(query)
-            context['products'] = Category.objects.get(slug=slug).products.filter(name=['Куртка', 'Вася'])
-            print(context['products'])
+        if len(query_dict) != 0:
+            for key in query_dict.keys():
+                print(key, '->>>>', query_dict[key])
+                if len(query_dict[key]) != 1:
+                    print('true')
+                    for item in query_dict[key]:
+                        filter = {key: item}
+                        search_model = CT_MODEL_MODEL_CLASS[slug].objects.filter(**filter)
+                        filter_results = filter_results & search_model
+                else:
+                    print('else')
+                    filter = {key: query_dict[key][0]}
+                    search_model = CT_MODEL_MODEL_CLASS[slug].objects.filter(**filter)
+                    filter_results = filter_results & search_model
+            context['products'] = filter_results
         else:
+            page_obj = Paginator(Category.objects.get(slug=slug).products.all(), 1)
             context['products'] = Category.objects.get(slug=slug).products.all()
-            print(context['products'])
+            context['page_obj'] = page_obj
+            # print(context['products'])
+        print(context)
+        
         return context
 
 # class FilterProductView(DetailView):
