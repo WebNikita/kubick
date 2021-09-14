1108f2427f66d6627eeb82cf59c1a14221c13d29
Froot <mistergame177@gmail.com>
2021-09-14 20:35:19 +0300

    Добавил ссылки для кнопки показать

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -121,9 +121,10 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
         img_url = {}
         pagintation_count = 6
 
-        print(int(self.request.GET.get('pagintation_count')))
+        
 
         if 'pagintation_count' in query_dict:
+            print(int(self.request.GET.get('pagintation_count')))
             pagintation_count = int(self.request.GET.get('pagintation_count'))
 
         if len(query_dict) != 0 and 'page' not in query_dict:
