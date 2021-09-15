1a7036b60ca37d8909509fd55892e317394bb217
Froot <mistergame177@gmail.com>
2021-09-15 16:40:58 +0300

    Добавил цикл для размеров

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -171,7 +171,7 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
                 context['img_url'] = img_url
             except Exception as e:
                 print(e)
-
+        print(kwargs['object'].size.split('\n'))
         # context['img_url'] = img_url
         # print(context['img_url'])
         
