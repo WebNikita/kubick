55bfdecf095e54bcd6e7488c6e9974b9ac2669b2
Froot <mistergame177@gmail.com>
2021-09-15 18:19:12 +0300

    Настроил выдачу товаров

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -124,7 +124,7 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
 
 
         if len(query_dict) != 0 and 'product_counter' in query_dict:
-            pagintation_count = query_dict['product_counter']
+            pagintation_count = int(query_dict['product_counter'])
 
         if len(query_dict) != 0 and 'page' not in query_dict and 'product_counter' not in query_dict:
             for key in query_dict.keys():
