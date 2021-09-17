df87e02a432d5b173b9bff9dcf86100ac97925bd
Froot <mistergame177@gmail.com>
2021-09-17 09:15:34 +0300

    Разделение товаров с разными размерами в корзине

--- CubickShop/cart/views.py
+++ CubickShop/cart/views.py
@@ -19,6 +19,7 @@ def cart_add(request, *args,**kwargs):
     content_type = ContentType.objects.get(model=ct_model)
     product = content_type.model_class().objects.get(slug=product_slug)
     cart_product = get_object_or_404(Product, id=product.id)
+    print(product_size)
     cart.add(product=cart_product, quantity=1, size=product_size)
     return redirect('cart:cart_detail')
 
