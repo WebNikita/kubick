0e525820008f72aafa0aeb74e324de84cd4645de
Froot <mistergame177@gmail.com>
2021-09-18 15:09:35 +0300

    проверка работы поиска с картинками

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -68,7 +68,7 @@ class SearchResultsView(ListView):
                 img_url[item.name] = bufer
             except Exception as e:
                 print(e)
-        return [object_list, img_url]
+        return {'object_list': object_list, "img_url": img_url}
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
