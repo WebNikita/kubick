81695c0cf76c882c28d2a18fbf1b77ab29d9b82f
Froot <mistergame177@gmail.com>
2021-10-01 12:19:48 +0300

    Editing import function

--- CubickShop/shop/resources.py
+++ CubickShop/shop/resources.py
@@ -33,7 +33,7 @@ class Summer_workwearResource(resources.ModelResource):
             id_bufer = i['id']
         
         for i in id_bufer:
-            print(Summer_workwear.objects.filter(i).image)
+            print(Summer_workwear.objects.filter(id=i))
         print('______')
             
         print(i['image'])
