4138584d671edffc28e36bfe792893fef323a84b
Proot <proot@Proot-desktop.Proot>
2021-10-21 00:42:50 +0300

    Счетчик цены

--- CubickShop/shop/static/js/bundle.js
+++ CubickShop/shop/static/js/bundle.js
@@ -14274,7 +14274,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
 
 const text = jquery__WEBPACK_IMPORTED_MODULE_0__('.price-for-one').prop('innerText');
-let inter = parseInt(text);
+const inter = parseInt(text);
 
 console.log(text);
 jquery__WEBPACK_IMPORTED_MODULE_0__('.plus').on('click', function() {
@@ -14351,9 +14351,9 @@ jquery__WEBPACK_IMPORTED_MODULE_0__(document).ready(function() {
     const container = jquery__WEBPACK_IMPORTED_MODULE_0__(this).parent().parent().parent()
     console.log('FIND' + container.find('.price-for-one'));
     if (container.find('.price-for-one')) {
-      inter = parseInt(container.find('.price-for-one').prop('innerText'));
+      const card = parseInt(container.find('.price-for-one').prop('innerText'));
       const value = jquery__WEBPACK_IMPORTED_MODULE_0__(this).val();
-      const price = value * inter;
+      const price = value * card;
       container.find('.end_price_product').html(price + ' Р');
     } else {
       const value = jquery__WEBPACK_IMPORTED_MODULE_0__(this).val();
