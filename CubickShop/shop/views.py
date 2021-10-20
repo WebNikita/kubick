a8ad1e0dcc2c632a863f30a50313b3e0e6d0f4e1
Froot <mistergame177@gmail.com>
2021-10-21 00:18:07 +0300

    Добавляю сортировку

--- CubickShop/shop/views.py
+++ CubickShop/shop/views.py
@@ -311,9 +311,8 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
         
         # Размер для формы размера
         for product in context['products']:
-            print(CT_MODEL_MODEL_CLASS[slug].objects.filter(name = product.name)[0].size)
             try:
-                bufer_product_size[product.name] = product.size.split('\n')
+                bufer_product_size[product.name] = CT_MODEL_MODEL_CLASS[slug].objects.filter(name = product.name)[0].size
             except Exception:
                 bufer_product_size[product.name] = '-'
         
