81de1a61688a0afebccee925e4d5539d3e5e4beb
Froot <mistergame177@gmail.com>
2021-10-21 22:28:18 +0300

    bugfix

--- CubickShop/cart/views.py
+++ CubickShop/cart/views.py
@@ -33,9 +33,8 @@ def cart_add(request, *args,**kwargs):
 @require_GET
 def add_product_count(request):
     get_data = request.GET
-    print('_______________',get_data.get('product_id'))
     cart = Cart(request)
-    product_id, count = f"{get_data.get('product_id')[0]}", get_data.get('count')[0]
+    product_id, count = f"{get_data['product_id'][0]}", get_data['count'][0]
     cart.product_edit_count(product_id,count)
 
     return redirect('cart:cart_detail')
