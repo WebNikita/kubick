0a28367dba7ba42dbc8c85f26445521c9741ed9f
Froot <mistergame177@gmail.com>
2021-10-26 12:23:22 +0300

    Добавил удаление после заказа

--- CubickShop/cart/views.py
+++ CubickShop/cart/views.py
@@ -89,8 +89,8 @@ def send_order_to_the_email(request, **kwargs):
         counter += 1
         message_body += f'{counter}. Наименование позиции: {cart[item]["product"].name}\nАртикул: {cart[item]["product"].article}\nКол-во: {cart[item]["quantity"]}\nРазмер: {cart[item]["size"]}\n-------------------\n'
     try:
-        cart.clear()
         send_mail('Успешный запрос',message_body, settings.EMAIL_HOST_USER, [user_info["email"]])
     except Exception as e:
         pass
+    cart.clear()
     return redirect('cart:cart_detail')
